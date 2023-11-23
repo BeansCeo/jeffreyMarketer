3e87efa5a79b6f3a49ff760836d2140cbea40364
Kilometer Immorales <alvaresstefano.23@gmail.com>
2023-11-23 15:15:09 +0530

    Added code to the js file

--- QuizGenerator/script.js
+++ QuizGenerator/script.js
@@ -1,4 +1,21 @@
-import {config} from 'dotenv';
-config();
-import {Configuration, OpenAIApi} from "openai"
-console.log(process.env.OPENAI_API_KEY);
+import { Axios } from "axios";
+const apiKey = process.env.OPENAI_API_KEY;
+const client = Axios.create({
+  headers:{
+    Authorization: "Bearer " + apiKey,
+  },
+})
+const params = {
+  prompt: "Do you like beans?",
+  model: "text-davinci-003",
+  max_tokens: 10,
+  temperature: 0,
+};
+client
+  .post("https://api.openai.com/v1/completions", params)
+  .then((result)=>{
+    console.log(result.data.choices[0].text); 
+  })
+  .catch((err) => {
+    console.log(err);
+  });
