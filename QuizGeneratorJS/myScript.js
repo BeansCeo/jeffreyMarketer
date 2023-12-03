d7a22e50feabac5ceded73cbd03e6c55d306836f
Kilometer Immorales <alvaresstefano.23@gmail.com>
2023-12-03 16:31:42 +0530

    updating js for function

--- QuizGeneratorJS/myScript.js
+++ QuizGeneratorJS/myScript.js
@@ -1,22 +1,33 @@
+const topic = document.getElementById("topic");
+const board = document.getElementById("schoolBoard");
+const diff = document.getElementById("level");
+const questions = document.getElementById("questions")
+const alert = document.getElementById("alert")
+const generate = document.getElementById("genQuiz")
 import axios from "axios";
-const apiKey = process.env.OPENAI_API_KEY;
-const client = axios.create({
-  headers:{
-    Authorization: "Bearer " + apiKey,
-  },
-})
-const params = {
-  prompt: "Do you like beans?",
-  model: "text-davinci-003",
-  max_tokens: 10,
-  temperature: 0,
-};
-client
-  .post("https://api.openai.com/v1/completions", params)
-  .then((result)=>{
-    console.log(result.data.choices[0].text); 
+while (topic == ""){
+    alert.textContent("Kindly provide a topic")
+}
+generate.addEventListener("click", function () {
+  const prompt = `Generate a quiz on ${topic}. You should include ${questions}. The difficulty of the questions should be based on ${board}, class ${diff}.`;
+  const apiKey = process.env.OPENAI_API_KEY;
+  const client = axios.create({
+    headers:{
+      Authorization: "Bearer " + apiKey,
+    },
   })
+  const params = {
+    prompt: `${prompt}`,
+    model: "text-davinci-003",
+    max_tokens: 10,
+    temperature: 0,
+  };
+  client
+    .post("https://api.openai.com/v1/completions", params)
+    .then((result)=>{
+      console.log(result.data.choices[0].text); 
+    })
   .catch((err) => {
-    console.log(err);
-  });
-
+      console.log(err);
+    });
+  }); 
