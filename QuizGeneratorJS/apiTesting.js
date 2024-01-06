bea9d375c9065aaaaa6154757baa84071c7b0932
Kilometer Immorales <alvaresstefano.23@gmail.com>
2024-01-06 18:14:17 +0530

    more testing
    trying to get website button request function to work properly.

---
+++ QuizGeneratorJS/apiTesting.js
@@ -0,0 +1,24 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: "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"
+});
+async function quiz() {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        "role": "user",
+        "content": "Create 5 analytical questions for an 8th grade igcse student, studying Photosynthesis and functions in a plant"
+      }
+    ],
+    temperature: 0.5,
+    max_tokens: 1024,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  const result = response.choices[0].message.content;
+  console.log(result);
+}
+quiz();
--- QuizGeneratorJS/script.js
+++ QuizGeneratorJS/script.js
@@ -1,8 +1,10 @@
 import OpenAI from "openai";
+const gen = document.getElementById("result");
+const generate = document.getElementById("generate")
 const openai = new OpenAI({
   apiKey: "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"
 });
-async function main() {
+generate.addEventListener('click', async () =>{
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
@@ -17,6 +19,10 @@ async function main() {
     frequency_penalty: 0,
     presence_penalty: 0,
   });
+  var delayInMilliseconds = 10000; //10 seconds
+  setTimeout(function() {
+  }, delayInMilliseconds);
   console.log(response.choices[0].message.content)
-}
-main();
+  const result = response.choices[0].message.content;
+  gen.textContent = `quiz: ${result}`
+});
